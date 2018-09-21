89fca6dda5758949c5af371ad04f9a10c310b535
KondaurovDev <kondaurov.dev@gmail.com>
2018-09-21 17:15:57 +0300

    remove

--- scalapb/project_deps/sh/scalapb_deps.sh
+++ scalapb/project_deps/sh/scalapb_deps.sh
@@ -2,20 +2,6 @@
 
 set -e
 
-#!/usr/bin/env sh
-
-set -e
-
-: "${project_name?Need to set project_name}"
-: "${org?Need to set org}"
-: "${version?Need to set version}"
-: "${realm?Need to set realm}"
-: "${host?Need to set host}"
-: "${repository?Need to set repository}"
-: "${login?Need to set login}"
-: "${password?Need to set password}"
-: "${src_path?Need to set src_path}"
-
 envsubst < "/scripts/build_template" > "build.sbt"
 
 sbt update
